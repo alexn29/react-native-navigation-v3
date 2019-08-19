243b87b57c613afee9d5a9a155dd0796c621bdf5
Alex Navarro <lexn29@gmail.com>
2019-08-19 14:46:04 -0600

    creating screens and combining navigators

--- App.js
+++ App.js
@@ -1,114 +1,165 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { 
+  createSwitchNavigator, 
+  createAppContainer, 
+  createDrawerNavigator, 
+  createBottomTabNavigator, 
+  createStackNavigator
+} 
+from "react-navigation";
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+  render() {
+    return (
+      <AppContainer />
+    );
+  }
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+class WelcomeScreen extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> WelcomeScreen </Text>
+        <Button title="Login" onPress={ () => this.props.navigation.navigate('Dashboard') } />
+      </View>
+    );
+  }
+}
+
+class DashboardScreen extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> DashboardScreen </Text>
+      </View>
+    );
+  }
+}
+
+class Feed extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Button title="Go To Detail Screen" onPress={() => this.props.navigation.navigate('Detail') }/>
+      </View>
+    );
+  }
+}
+
+class Detail extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Detail</Text>
+      </View>
+    );
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Profile </Text>
+      </View>
+    );
+  }
+}
+
+class Settings extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Settings </Text>
+      </View>
+    );
+  }
+}
+
+const FeedStack = createStackNavigator({
+  Feed: {
+    screen: Feed,
+    navigationOptions: ({navigation}) => {
+      return {
+        headerTitle: 'Feed',
+        headerLeft: <Icon style={{marginLeft: 10}} onPress={() => navigation.openDrawer() } name="md-menu" size={30} color="#900" />
+      }
+    }
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  Detail: {
+    screen: Detail
+  }
+})
+
+const ProfileStack = createStackNavigator({
+  Profile: {
+    screen: Profile,
+    navigationOptions: ({navigation}) => {
+      return {
+        headerTitle: 'Profile',
+        headerLeft: <Icon style={{marginLeft: 10}} onPress={() => navigation.openDrawer() } name="md-menu" size={30} color="#900" />
+      }
+    }
+  }
+})
+
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: Settings,
+    navigationOptions: ({navigation}) => {
+      return {
+        headerTitle: 'Settings',
+        headerLeft: <Icon style={{marginLeft: 10}} onPress={() => navigation.openDrawer() } name="md-menu" size={30} color="#900" />
+      }
+    }
+  }
+})
+
+const DashboardTabNavigator = createBottomTabNavigator({
+  Feed: FeedStack, 
+  Profile: ProfileStack, 
+  Settings: SettingsStack
+}, {
+  navigationOptions: ({navigation}) => {
+    const {routeName} = navigation.state.routes[navigation.state.index];
+    return {
+      header: null,
+      headerTitle: routeName
+    }
+  }
+})
+
+const DashboardStackNavigator = createStackNavigator({
+  DashboardTabNavigator: DashboardTabNavigator
+}, {
+  defaultNavigationOptions: ({navigation}) => {
+    return {
+      headerLeft: <Icon style={{marginLeft: 10}} onPress={() => navigation.openDrawer() } name="md-menu" size={30} color="#900" />
+    }
+  }
+})
+
+const AppDrawerNavigation = createDrawerNavigator({
+  dashboard: {
+    screen: DashboardStackNavigator
+  }
+})
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome: { screen: WelcomeScreen },
+  Dashboard: { screen: AppDrawerNavigation }
+})
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
 export default App;
